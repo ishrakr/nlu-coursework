c8789551befc80b42a0ec039a6036d8b4df9bb27
Ishrak Rahman <admin@ishrak.xyz>
2025-04-29 21:09:54 +0000

    Update CSS_225/M3/P6

--- CSS_225/Module03/problem06.py
+++ CSS_225/Module03/problem06.py
@@ -2,10 +2,10 @@
 # Last Edited: 4/29/2025
 # 
 # Description:
-# A scrpt that converts the temperature from Farenheit to Celcius
+# A scrpt that converts the temperature from Farenheit to Celsius
 
 #!/usr/bin/env python
 
 temp_in_farenheit = float(input("Please enter the temperature in Farenheit: "))
 temp_in_celsius = ((temp_in_farenheit - 30) / 2)
-print(temp_in_celsius)
+print(temp_in_celsius, "C")
